4577ce859678969c0537bfefcbed4547f32f4b47
vlade11115 <vlade11115@gmail.com>
2019-02-08 14:45:03 +0200

    Complete parsing.
     - add lesson info.

--- knu_parser/pipelines.py
+++ knu_parser/pipelines.py
@@ -5,17 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from scrapy.exceptions import DropItem
-
 
 class KnuParserPipeline(object):
-    pass
-    # def __init__(self):
-    #     self.ids_seen = set()
-
-    # def process_item(self, item, spider):
-    #     if item['group_id'] in self.ids_seen:
-    #         raise DropItem("Duplicate item found: %s" % item)
-    #     else:
-    #         self.ids_seen.add(item['group_id'])
-    #         return item
+    def process_item(self, item, spider):
+        return item
--- knu_parser/spiders/schedule.py
+++ knu_parser/spiders/schedule.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from scrapy import Spider, http, shell, exceptions
+from scrapy import Spider, http
 
 from knu_parser.items import KnuParserItem
 
@@ -21,16 +21,9 @@ class ScheduleSpider(Spider):
         """
         for faculty in response.xpath('//*[@id="TimeTableForm_faculty"]/option')[1:]:
             faculty_id = faculty.xpath('@value').get()
-            if not faculty_id:
-                # self.logger.debug('Failed to parse faculty id for %s faculty_id', faculty_id)
-                # shell.inspect_response(response, self)
-                # raise exceptions.CloseSpider('')
-                continue
             faculty_name = faculty.xpath('text()').get()
-            item = KnuParserItem()
             form_data = {'TimeTableForm[faculty]': faculty_id}
-            item['faculty_id'] = faculty_id
-            item['faculty_name'] = faculty_name
+            item = KnuParserItem({'faculty_id': faculty_id, 'faculty_name': faculty_name})
             request = http.FormRequest(url=response.url, formdata=form_data, callback=self.parse_course)
             request.meta['item'] = item
             yield request
@@ -39,21 +32,13 @@ class ScheduleSpider(Spider):
         """
         Parse id of course.
         """
-        for course in response.xpath('//*[@id="TimeTableForm_course"]/option')[1:]:
-            course_id = course.xpath('@value').get()
-            if not course_id:
-                self.logger.debug('Failed to parse id of course for %s course_id', course_id)
-                # shell.inspect_response(response, self)
-                # raise exceptions.CloseSpider('')
-                continue
-
-            item = response.meta['item']
+        for course_id in response.xpath('//*[@id="TimeTableForm_course"]/option/@value')[1:].getall():
+            item = response.meta['item'].copy()
             item['course_id'] = course_id
             form_data = {
                 'TimeTableForm[faculty]': item['faculty_id'],
                 'TimeTableForm[course]': item['course_id'],
             }
-            self.logger.debug('parse group %s', form_data)
             request = http.FormRequest(url=response.url, formdata=form_data, callback=self.parse_group)
             request.meta['item'] = item
             yield request
@@ -63,15 +48,10 @@ class ScheduleSpider(Spider):
         Parse id and name of group.
         """
         for group in response.xpath('//*[@id="TimeTableForm_group"]/option')[1:]:
+            item = response.meta['item'].copy()
             group_id = group.xpath('@value').get()
             group_name = group.xpath('text()').get()
-            if (not group_id) or (not group_name):
-                self.logger.debug('Failed to parse id or info for %s group_id %s group_name', group_id, group_name)
-                shell.inspect_response(response, self)
-                raise exceptions.CloseSpider('')
-                continue
 
-            item = response.meta['item']
             item['group_id'] = group_id
             item['group_name'] = group_name
             form_data = {
@@ -79,38 +59,49 @@ class ScheduleSpider(Spider):
                 'TimeTableForm[course]': item['course_id'],
                 'TimeTableForm[group]': item['group_id'],
             }
-            self.logger.debug('parse group %s', form_data)
-            # request = http.FormRequest(url=response.url, formdata=form_data, callback=self.parse_schedule)
-            # request.meta['item'] = item
-            # yield request
+            request = http.FormRequest(url=response.url, formdata=form_data, callback=self.parse_schedule)
+            request.meta['item'] = item
+            yield request
 
     def parse_schedule(self, response):
         """
         Parse actual schedule of group.
         """
-        item = response.meta['item']
         table = response.xpath('//*[@id="timeTableGroup"]/tr')
-
+        item = response.meta['item'].copy()
         for row in table:
             # cycle for day of week
             lessons_info = row.xpath('./td/div[@class="mh-50 cell cell-vertical"]/span[1]/text()').getall()
+            lessons_start = row.xpath(
+                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="start"]/text()').getall()
+            lessons_finish = row.xpath(
+                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="finish"]/text()').getall()
             day_of_week = row.xpath('./td/div/text()').get()
             item['day_of_week'] = day_of_week
             for day in row.xpath('./td')[1:]:
                 date = day.xpath('./div/text()').get()
                 item['date'] = date
                 lessons = day.xpath('./div[@class="cell mh-50"]')
-                for index, lesson in enumerate(lessons):
-                    item['lesson_number'] = lessons_info[index]
+                if not lessons:
+                    self.logger.debug('Skip empty day %s', item)
+                    continue
+                count = 0
+                for lesson in lessons:
+                    item['lesson_number'] = lessons_info[count].split()[0]
+                    item['lesson_start'] = lessons_start[count]
+                    item['lesson_finish'] = lessons_finish[count]
+                    count += 1
+                    if count == len(lessons_info) - 1:
+                        count = 0
                     lesson_tag_value = lesson.xpath('./@data-content').get()
                     if not lesson_tag_value:
-                        self.logger.debug('Skip empty cell for group %s faculcy %s at %s ', item['group_name'],
-                                          item['faculty_name'], date)
+                        self.logger.debug('Skip empty lesson %s', item)
                         continue
                     lesson = list(filter(None, map(str.strip, lesson_tag_value.split('<br>'))))
-                    item['discipline'] = lesson[0]
+                    discipline = lesson[0]
+                    item['discipline'] = discipline[:discipline.find('[')]
+                    item['lesson_type'] = discipline[discipline.find('[') + 1:discipline.find(']')]
                     item['audience'] = lesson[1].split('-', 1)[1]
                     item['corpus_number'] = lesson[1].split('-', 1)[0].split('. ', 1)[1]
                     item['lecturer'] = lesson[2]
-                    item['added_at'] = lesson[3]
                     yield item
