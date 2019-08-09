6131d885375ef60cf667c57158692cbd910da151
vlade11115 <vlade11115@gmail.com>
2019-08-09 11:47:52 +0300

    Bump dates for parser.

--- knu_parser/spiders/schedule.py
+++ knu_parser/spiders/schedule.py
@@ -8,90 +8,111 @@ from knu_parser.items import KnuParserItem
 
 
 class ScheduleSpider(Spider):
-    name = 'schedule'
-    allowed_domains = ['asu.knu.edu.ua']
-    start_urls = [
-        'http://asu.knu.edu.ua/timeTable/group',
-    ]
+    name = "schedule"
+    allowed_domains = ["asu.knu.edu.ua"]
+    start_urls = ["http://asu.knu.edu.ua/timeTable/group"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        start_date = parse(getattr(self, 'start_time', str(datetime.date.today())))
-        end_time = parse(getattr(self, 'end_time', str(start_date + datetime.timedelta(days=13))))
+        start_date = parse(getattr(self, "start_time", str(datetime.date.today())))
+        end_time = parse(
+            getattr(self, "end_time", str(start_date + datetime.timedelta(weeks=8)))
+        )
         self.dates = {
-            'TimeTableForm[date1]': start_date.strftime('%d.%m.%Y'),
-            'TimeTableForm[date2]': end_time.strftime('%d.%m.%Y'),
+            "TimeTableForm[date1]": start_date.strftime("%d.%m.%Y"),
+            "TimeTableForm[date2]": end_time.strftime("%d.%m.%Y"),
         }
-        self.logger.debug('Start crawl with given dates %r', self.dates)
+        self.logger.debug("Start crawl with given dates %r", self.dates)
 
     def parse(self, response):
         """
         Parse initial page, take faculty id and name.
         """
         for faculty in response.xpath('//*[@id="TimeTableForm_faculty"]/option')[1:]:
-            faculty_id = faculty.xpath('@value').get()
-            faculty_name = faculty.xpath('text()').get()
-            form_data = {'TimeTableForm[faculty]': faculty_id}
-            item = KnuParserItem({'faculty_id': faculty_id, 'faculty_name': faculty_name})
-            yield FormRequest(url=response.url, formdata=form_data, callback=self.parse_course,
-                              meta={'item': item})
+            faculty_id = faculty.xpath("@value").get()
+            faculty_name = faculty.xpath("text()").get()
+            form_data = {"TimeTableForm[faculty]": faculty_id}
+            item = KnuParserItem(
+                {"faculty_id": faculty_id, "faculty_name": faculty_name}
+            )
+            yield FormRequest(
+                url=response.url,
+                formdata=form_data,
+                callback=self.parse_course,
+                meta={"item": item},
+            )
 
     def parse_course(self, response):
         """
         Parse id of course.
         """
-        for course_id in response.xpath('//*[@id="TimeTableForm_course"]/option/@value')[1:].getall():
-            item = response.meta['item'].copy()
-            item['course_id'] = course_id
+        for course_id in response.xpath(
+            '//*[@id="TimeTableForm_course"]/option/@value'
+        )[1:].getall():
+            item = response.meta["item"].copy()
+            item["course_id"] = course_id
             form_data = {
-                'TimeTableForm[faculty]': item['faculty_id'],
-                'TimeTableForm[course]': item['course_id'],
+                "TimeTableForm[faculty]": item["faculty_id"],
+                "TimeTableForm[course]": item["course_id"],
             }
-            yield FormRequest(url=response.url, formdata=form_data, callback=self.parse_group, meta={'item': item})
+            yield FormRequest(
+                url=response.url,
+                formdata=form_data,
+                callback=self.parse_group,
+                meta={"item": item},
+            )
 
     def parse_group(self, response):
         """
         Parse id and name of group.
         """
         for group in response.xpath('//*[@id="TimeTableForm_group"]/option')[1:]:
-            item = response.meta['item'].copy()
-            group_id = group.xpath('@value').get()
-            group_name = group.xpath('text()').get()
+            item = response.meta["item"].copy()
+            group_id = group.xpath("@value").get()
+            group_name = group.xpath("text()").get()
 
-            item['group_id'] = group_id
-            item['group_name'] = group_name
+            item["group_id"] = group_id
+            item["group_name"] = group_name
             form_data = {
-                'TimeTableForm[faculty]': item['faculty_id'],
-                'TimeTableForm[course]': item['course_id'],
-                'TimeTableForm[group]': item['group_id'],
+                "TimeTableForm[faculty]": item["faculty_id"],
+                "TimeTableForm[course]": item["course_id"],
+                "TimeTableForm[group]": item["group_id"],
                 **self.dates,
             }
-            yield FormRequest(url=response.url, formdata=form_data, callback=self.parse_schedule,
-                              meta={'item': item})
+            yield FormRequest(
+                url=response.url,
+                formdata=form_data,
+                callback=self.parse_schedule,
+                meta={"item": item},
+            )
 
     def parse_schedule(self, response):
         """
         Parse actual schedule of group.
         """
         table = response.xpath('//*[@id="timeTableGroup"]/tr')
-        item = response.meta['item'].copy()
+        item = response.meta["item"].copy()
 
         for row in table:
             # cycle for day of week
-            lessons_info = row.xpath('./td/div[@class="mh-50 cell cell-vertical"]/span[1]/text()').getall()
+            lessons_info = row.xpath(
+                './td/div[@class="mh-50 cell cell-vertical"]/span[1]/text()'
+            ).getall()
             lessons_start = row.xpath(
-                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="start"]/text()').getall()
+                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="start"]/text()'
+            ).getall()
             lessons_finish = row.xpath(
-                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="finish"]/text()').getall()
-            day_of_week = row.xpath('./td/div/text()').get()
-            item['day_of_week'] = day_of_week
+                './td/div[@class="mh-50 cell cell-vertical"]/span[@class="finish"]/text()'
+            ).getall()
+            day_of_week = row.xpath("./td/div/text()").get()
+            item["day_of_week"] = day_of_week
             week_number = 1
 
-            days = row.xpath('./td')[1:]
+            days = row.xpath("./td")[1:]
             for index, day in enumerate(days):
-                item['date'] = day.xpath('./div/text()').get()
-                item['week_number'] = week_number
+                item["date"] = day.xpath("./div/text()").get()
+                item["week_number"] = week_number
 
                 if not (index % len(days)):
                     week_number = 2 if week_number == 1 else 1
@@ -99,26 +120,30 @@ class ScheduleSpider(Spider):
                 lessons = day.xpath('./div[@class="cell mh-50"]')
 
                 if not lessons:
-                    self.logger.debug('Skip empty day %s', item)
+                    self.logger.debug("Skip empty day %s", item)
                     continue
 
                 count = 0
                 for lesson in lessons:
-                    item['lesson_number'] = lessons_info[count].split()[0]
-                    item['lesson_start'] = lessons_start[count]
-                    item['lesson_finish'] = lessons_finish[count]
+                    item["lesson_number"] = lessons_info[count].split()[0]
+                    item["lesson_start"] = lessons_start[count]
+                    item["lesson_finish"] = lessons_finish[count]
                     count += 1
                     if count == len(lessons_info):
                         count = 0
-                    lesson_tag_value = lesson.xpath('./@data-content').get()
+                    lesson_tag_value = lesson.xpath("./@data-content").get()
                     if not lesson_tag_value:
-                        self.logger.debug('Skip empty lesson %s', item)
+                        self.logger.debug("Skip empty lesson %s", item)
                         continue
-                    lesson = list(filter(None, map(str.strip, lesson_tag_value.split('<br>'))))
+                    lesson = list(
+                        filter(None, map(str.strip, lesson_tag_value.split("<br>")))
+                    )
                     discipline = lesson[0]
-                    item['discipline'] = discipline[:discipline.find('[')]
-                    item['lesson_type'] = discipline[discipline.find('[') + 1:discipline.find(']')]
-                    item['audience'] = lesson[2].split('-', 1)[1]
-                    item['corpus_number'] = lesson[2].split('-', 1)[0].split('. ', 1)[1]
-                    item['lecturer'] = lesson[3]
+                    item["discipline"] = discipline[: discipline.find("[")]
+                    item["lesson_type"] = discipline[
+                        discipline.find("[") + 1 : discipline.find("]")
+                    ]
+                    item["audience"] = lesson[2].split("-", 1)[1]
+                    item["corpus_number"] = lesson[2].split("-", 1)[0].split(". ", 1)[1]
+                    item["lecturer"] = lesson[3]
                     yield item
